090ca9cca9108397a1d8b1c1f3af895d84525380
zy <1457715339@qq.com>
2020-02-13 16:55:53 +0800

    完成City中的搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.search
+  height 0.72rem
+  background $bgColor
+  padding 0.1rem 0.1rem
+  .search-input
+    width 100%
+    height 100%
+    line-height 0.62rem
+    text-align center
+    border-radius 0.06rem
+    color #666
+    padding 0 0.1rem
+    box-sizing border-box
+</style>
