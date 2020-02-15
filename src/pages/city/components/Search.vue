4c374c0896dd62d0d9dbdcdf34438fd9f3ed34e4
zy <1457715339@qq.com>
2020-02-15 23:47:53 +0800

    实现搜索框逻辑

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,55 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市或拼音" />
+  <div>
+    <div class="search">
+      <input type="text" class="search-input" placeholder="输入城市或拼音" v-model="keyword" />
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li v-show="hasNoData">找不到你想去的城市呢（｡ò ∀ ó｡）</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from "better-scroll";
 export default {
-  name: "CitySearch"
+  name: "CitySearch",
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: "",
+      list: []
+    };
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length;
+    }
+  },
+  watch: {
+    keyword() {
+      // 监听搜索框内的文字变化
+      const result = []; // !!这个声明非常重要，每次keyword发生变化都会重置result为空
+      for (const i in this.cities) {
+        this.cities[i].forEach(value => {
+          if (
+            value.spell.indexOf(this.keyword) === 0 ||
+            value.name.indexOf(this.keyword) === 0
+          ) {
+            result.push(value);
+          }
+        });
+      }
+      this.list = this.keyword ? result : []; // keyword不存在就直接清空list
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.search);
+  }
 };
 </script>
 
@@ -25,4 +68,18 @@ export default {
     color #666
     padding 0 0.1rem
     box-sizing border-box
+.search-content
+  overflow hidden
+  position absolute
+  z-index 1
+  top 1.75rem
+  right 0
+  bottom 0
+  left 0
+  background #ddd
+  li
+    line-height 0.62rem
+    padding-left 0.2rem
+    background #fff
+    color #666
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const mockdata1 = require('./mock/home.json');
-const mockdata2 = require('./mock/city.json');
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
@@ -13,14 +11,15 @@ module.exports = {
       .set('imgs', resolve('src/assets/imgs'))
   },
   devServer: {
-    port: 8080,
-    before(app) {
-      app.get('/api/home.json', (req, res, next) => {
-        res.json(mockdata1);
-      })
-      app.get('/api/city.json', (req, res, next) => {
-        res.json(mockdata2);
-      })
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/mock'
+        }
+      }
     }
   }
 }
